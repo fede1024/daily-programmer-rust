9a72cce24f5cd12ed8ccef3928fc117736ea4c2d
Federico Giraud <giraud.federico@gmail.com>
2016-04-21 09:07:21 +0100

    Done

--- 260_hard/src/main.rs
+++ 260_hard/src/main.rs
@@ -3,15 +3,63 @@ use std::env;
 mod map;
 
 use map::Map;
+use map::Tile;
 use map::parse_input_file;
+
+
+fn find_path(map: &mut Map, depth: i32) -> Option<Vec<(char, i32, i32)>> {
+    if depth == 0 {
+        return None
+    }
+    for &(m, dy, dx) in [('↑', -1, 0), ('↓', 1, 0), ('←', 0, -1), ('→', 0, 1)].iter() {
+        let head = map.head;
+        let y = map.head.0 + dy;
+        let x = map.head.1 + dx;
+        if let Some(t) = map.get(y, x) {
+            if t == Tile::Pit || t == Tile::Snake {
+                continue;
+            }
+            if t == Tile::Food {
+                map.food -= 1;
+                if map.food == 0 {
+                    return Some(vec![(m, y, x)]);
+                }
+            }
+            map.head.0 = y;
+            map.head.1 = x;
+            map.set(y, x, Tile::Snake);
+            if let Some(mut res) = find_path(map, depth - 1) {
+                res.push((m, y, x));
+                return Some(res);
+            }
+            map.set(y, x, t);
+            if t == Tile::Food {
+                map.food += 1;
+            }
+            map.head = head;
+        }
+    }
+    None
+}
 
 fn main() {
     let path = env::args().nth(1).expect("Missing arg");
-    println!("{:?}", path);
     let maps = parse_input_file(path.as_str()).expect("File not readable");
 
-    for map in maps {
-        println!("{:?}", map.start);
+    for (n, m) in maps.iter().enumerate() {
+        let map = m.clone();
+        println!("\nMap: {}", n);
         map.print();
+        println!("{}x{}: {} (h: {:?})", map.height, map.width, map.food, map.head);
+
+        for d in 0..(map.height * map.width) {
+            let mut map = map.clone();
+            let res = find_path(&mut map, d);
+            if let Some(mut path) = res {
+                path.reverse();
+                map.print_with_path(path);
+                break;
+            }
+        }
     }
 }
--- 260_hard/src/map.rs
+++ 260_hard/src/map.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum ParseError {
@@ -32,7 +33,7 @@ pub enum Tile {
 #[derive(Clone)]
 pub struct Map {
     data: Vec<Tile>,
-    pub start: (i32, i32),
+    pub head: (i32, i32),
     pub width: i32,
     pub height: i32,
     pub food: i32,
@@ -40,7 +41,7 @@ pub struct Map {
 
 impl Map {
     pub fn new(width: i32, height: i32) -> Map {
-        Map { data: vec![], width: width, height: height, start: (0, 0), food: 0 }
+        Map { data: vec![], width: width, height: height, head: (0, 0), food: 0 }
     }
 
     pub fn get(&self, y: i32, x: i32) -> Option<Tile> {
@@ -56,6 +57,11 @@ impl Map {
     }
 
     pub fn print(&self) {
+        self.print_with_path(vec![]);
+    }
+
+    pub fn print_with_path(&self, path: Vec<(char, i32, i32)>) {
+        let path_set: HashMap<(i32, i32), char> = path.iter().map(|&(c, y, x)| ((y, x), c)).collect();
         let print_border = || {
             print!("+");
             for _ in 0..self.width { print!("-"); }
@@ -71,7 +77,8 @@ impl Map {
                     Tile::Food => '*',
                     Tile::Pit => 'O',
                 };
-                print!("{}", c);
+                let x = path_set.get(&(y, x)).unwrap_or(&c);
+                print!("{}", x);
             }
             println!("|");
         }
@@ -85,12 +92,16 @@ fn parse_maps(lines: Vec<String>) -> Result<Vec<Map>, ParseError> {
 
     for line in lines {
         let chars: Vec<char> = line.chars().collect();
+        if chars.len() < 1 {
+            continue
+        }
         if chars[0] == '+' {
             if line.as_str().contains("map") {
                 map = Map::new((chars.len() - 2) as i32, 0);
             } else {
-                let n = map.data.iter().position(|&t| t == Tile::Snake).unwrap_or(0) as i32;
-                map.start = (n / map.width, n % map.width);
+                let h = map.data.iter().position(|&t| t == Tile::Snake).unwrap_or(0) as i32;
+                map.head = (h / map.width, h % map.width);
+                map.food = map.data.iter().filter(|&t| *t == Tile::Food).count() as i32;
                 maps.push(map.clone());
             }
             continue;
